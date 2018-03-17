5364ac428fde1db255ee6c471176bd0cc8316550
JOSHDESKTOP <joshtk1994@gmail.com>
2018-03-17 11:04:50 +1000

    Sites unlocking w player collision

--- Gardens2/Assets/Scripts/MainMenu.cs
+++ Gardens2/Assets/Scripts/MainMenu.cs
@@ -5,8 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	public void PlayGame ()
+    public string sceneName;
+	public void LoadScene ()
     {
-        SceneManager.LoadScene("Map");
+        SceneManager.LoadScene(sceneName);
     }
 }
---
+++ Gardens2/Assets/Scripts/UnlockSite.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockSite : MonoBehaviour {
+
+    public GameObject SiteButton;
+    public GameObject TempSiteHolder;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if(other.gameObject.tag == "Player")
+        //{
+            TempSiteHolder.SetActive(false);
+            SiteButton.SetActive(true);
+
+    }
+}
